572455521f41fcb3e3c4dc8bb3872bdf0b663978
Hyounkyu <97646070+okorion@users.noreply.github.com>
2025-03-01 02:21:37 +0900

    feat: community, meals, share 페이지 추가

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,9 @@
+export default function MealSlugPages({ params }) {
+  return (
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Welcome to the meal: {params.slug}!
+      </h1>
+    </main>
+  );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,9 @@
+export default function SharePage() {
+  return (
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>
+        Share your meals with the world!
+      </h1>
+    </main>
+  );
+}
