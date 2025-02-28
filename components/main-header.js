8a08e3cf13bce281606d29bc7313777a4b161028
Hyounkyu <97646070+okorion@users.noreply.github.com>
2025-03-01 02:47:30 +0900

    style: main header 컴포넌트 스타일 추가

--- components/main-header.js
+++ components/main-header.js
@@ -2,16 +2,23 @@ import Link from "next/link";
 import Image from "next/image";
 
 import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
-        <Image src={logoImg.src} alt="A plate with food on it" width={150} height={150} priority/>
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        <Image
+          src={logoImg.src}
+          alt="A plate with food on it"
+          width={80}
+          height={80}
+          priority
+        />
         NextLevel Food
       </Link>
 
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
