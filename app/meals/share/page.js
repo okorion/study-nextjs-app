208a663d44a3fe1e697aae3e5e07a824a5a45dea
okorion <ok.or.orion@gmail.com>
2025-03-01 21:04:11 +0900

    feat: meal submit 버튼 pending 상태 추가

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,4 +1,5 @@
 import ImagePicker from '@/components/meals/image-picker';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import { shareMeal } from '@/lib/action';
 
 import classes from './page.module.css';
@@ -38,7 +39,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
