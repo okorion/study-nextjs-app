3942ab05d28e8a89f338dc048fbaba65d7205b25
okorion <ok.or.orion@gmail.com>
2025-03-01 19:05:57 +0900

    feat: meals 페이지 error 처리 추가

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,17 @@
+import { Suspense } from "react";
+
 import Link from "next/link";
 
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
 import classes from "./page.module.css";
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
 
 export default async function MealsPage() {
   const meals = await getMeals();
@@ -23,7 +31,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
